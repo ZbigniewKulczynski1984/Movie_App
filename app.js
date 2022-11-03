9bdb289f9336ff8f888513850d40615ca8da0fc4
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-03 12:37:08 +0100

    code added to app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 //Klucz API (v3 auth) 3712af6cd6e5f73254bafa21019679fe
 
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3712af6cd6e5f73254bafa21019679fe&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=3712af6cd6e5f73254bafa21019679fe&query="'
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data.results)
+}
