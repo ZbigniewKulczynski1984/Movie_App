847b0ac431c37cee62b859b4cd0b0de2cd271645
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-03 13:08:02 +0100

    code added to app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3712af6cd6e5f73254bafa21019679fe&query="'
 
-const main = docement.getElementById('main')
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getAnimations('search')
 
@@ -21,24 +21,35 @@ function showMovies(movies) {
     main.innerHTML = ''
 
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overwwiev} = movie
+        const { title, poster_path, vote_average, overview} = movie
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
 
         movieEl.innerHTML = `
-        <div class="movie">
-            <img src="https://pixabay.com/get/gf7ac7d7d547ea55ae05ed83f8663bcee8d7db477f2fc7d7772cff7ec3eff84c94261ec3a053316353602c1591e4fce407218cbd541827d4c1a704383bdafae689d20322294198aced68d8c83ced90ee2_1920.jpg" alt="">
+        
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
                 <h3>Overview</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo perferendis ab dignissimos officiis, quasi exercitationem.</p>
+                ${overview}
             </div>
-        </div>`
+        `
+        main.appendChild(movieEl)
     })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8)  {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) => {
