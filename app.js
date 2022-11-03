c5b1635b19a24645e5ee593164a019c6e31f8517
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-03 12:27:30 +0100

    api_key got after register

---
+++ app.js
@@ -0,0 +1,3 @@
+//Klucz API (v3 auth) 3712af6cd6e5f73254bafa21019679fe
+
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3712af6cd6e5f73254bafa21019679fe&page=1'
