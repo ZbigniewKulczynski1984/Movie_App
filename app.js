6177475465ccaed2541824264202d59415685504
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-03 13:10:46 +0100

    bug removed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //Klucz API (v3 auth) 3712af6cd6e5f73254bafa21019679fe
 
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3712af6cd6e5f73254bafa21019679fe&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3712af6cd6e5f73254bafa21019679fe'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3712af6cd6e5f73254bafa21019679fe&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
-const search = document.getAnimations('search')
+const search = document.getElementById('search')
 
 getMovies(API_URL)
 
